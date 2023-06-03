4952c04e2643afd884bbec13311d6e1e68ddd500
GiovannaAlonso <106785638+Alonsogio@users.noreply.github.com>
2023-06-02 22:46:57 -0300

    delete img

--- sweet-cake/src/data/database.ts
+++ sweet-cake/src/data/database.ts
@@ -2,10 +2,10 @@
 export const data = [
   {
     id: 1,
-    img: 'assets/img/bolofv.png',
-    img2: 'assets/img/fv1.jpg',
-    img3: 'assets/img/fv2.jpg',
-    img4: 'assets/img/fv3.jpg',
+    img: 'https://drive.google.com/uc?id=1AZnxAShwJ3_Rs9F6GVCt0eSWg0z1_S_k',
+    img2: 'https://drive.google.com/uc?id=1bfpl-CvI_Gr65mkpIe4mM89I9gZh_WDq',
+    img3: 'https://drive.google.com/uc?id=1G-0pO0Ev_aN33LYmIQIg5FT7XvHbWJ8e',
+    img4: 'https://drive.google.com/uc?id=1t3bNr0cqUOIaUzzNDT9apdQZnRcRjvHt',
     nameItem: 'Bolo Frutas Vermelhas',
     description:
       'A aparência desse bolo é geralmente irresistível. A superfície do bolo é coberta com uma camada generosa de creme de chantilly ou um glacê suave, que serve como base para a decoração com as frutas vermelhas. As frutas são distribuídas de forma a criar uma combinação harmoniosa de cores vibrantes e texturas.\n\n' +
@@ -17,10 +17,10 @@ export const data = [
   },
   {
     id: 2,
-    img: 'assets/img/tortadelimao.png',
-    img2: 'assets/img/Lm1.jpg',
-    img3: 'assets/img/Lm2.jpg',
-    img4: 'assets/img/Lm3.jpg',
+    img: 'https://drive.google.com/uc?id=10_wcmeS686R4TgolVBV_M2XyUjFbtnI6',
+    img2: 'https://drive.google.com/uc?id=1sBW65bzX8jYwfGMogiyi1YZiYYZ_ZwI_',
+    img3: 'https://drive.google.com/uc?id=1y6Npjns8smXsIl_bGcMehdgBKo7aUYXF',
+    img4: 'https://drive.google.com/uc?id=1HefXQOiQBfBro8ZYat0QXj-cHxRe1L_U',
     nameItem: 'Bolo de Limão',
     description:
       'O recheio é preparado com suco de limão fresco, raspas de limão e açúcar, resultando em um creme suave e azedinho. Esse recheio é espalhado entre as camadas do bolo, proporcionando um contraste de sabores entre o doce da massa e o azedinho do limão.  Para complementar, o bolo de limão é coberto com uma cobertura de limão, que pode ser feita com uma mistura de suco de limão e açúcar de confeiteiro. Essa cobertura adiciona um toque final de acidez e uma camada de sabor irresistível ao bolo. Além disso, o bolo de limão pode ser decorado com raspas de limão ou fatias finas da fruta, adicionando um toque visualmente atrativo e realçando ainda mais o aroma e o sabor do limão. O resultado final é um bolo de limão com uma combinação perfeita de doce e azedo, ideal para os amantes de sobremesas refrescantes e cítricas. É uma opção perfeita para qualquer ocasião, desde festas de aniversário até um simples lanche da tarde.',
@@ -30,10 +30,10 @@ export const data = [
   },
   {
     id: 3,
-    img: 'assets/img/cake2.png',
-    img2: 'assets/img/rv1.jpg',
-    img3: 'assets/img/rv2.jpg',
-    img4: 'assets/img/rv3.png',
+    img: 'https://drive.google.com/uc?id=1IqUooIW9GqQBB3EkEGGEprWRwgqL5zqb',
+    img2: 'https://drive.google.com/uc?id=1dq54QoLE5opesFpkxCLosNMfOv9i-h6y',
+    img3: 'https://drive.google.com/uc?id=1qTyT-U9rEIc12VBZrzV1AfFeqKVaGza9',
+    img4: 'https://drive.google.com/uc?id=165cOpR0SLnpWh34hFyzmxWK4eSlZx7sv',
     nameItem: 'Bolo Red Velvet',
     description:
       'A massa do bolo é feita com ingredientes como farinha de trigo, açúcar, manteiga, cacau em pó e um toque de corante vermelho, que dá ao bolo sua cor vibrante. Além disso, a massa leva um pouco de vinagre branco, que ajuda a realçar o sabor e a textura do bolo. O Red Velvet é geralmente acompanhado de um creme de queijo como cobertura, que adiciona um toque de cremosidade e suavidade ao bolo. O creme de queijo é feito com queijo cremoso, manteiga, açúcar de confeiteiro e baunilha, resultando em uma combinação perfeita de sabores com o bolo. A decoração do bolo Red Velvet pode variar, mas é comum encontrá-lo decorado com migalhas de bolo, raspas de chocolate branco ou até mesmo com uma camada adicional de creme de queijo por cima.',
@@ -43,10 +43,10 @@ export const data = [
   },
   {
     id: 4,
-    img: 'assets/img/blueberrycupcakes.png',
-    img2: 'assets/img/bb1.jpg',
-    img3: 'assets/img/bb2.jpg',
-    img4: 'assets/img/bb3.jpg',
+    img: 'https://drive.google.com/uc?id=11iKvOgVrhkCetQn1u-ogfz_uuXhHI0Ly',
+    img2: 'https://drive.google.com/uc?id=1zHRw6Apb7OhRyFv4-C2EadU2gISFQKt1',
+    img3: 'https://drive.google.com/uc?id=1xcTvK6C-aLOE9SwXt3WSjZ8ZGbRzFp10',
+    img4: 'https://drive.google.com/uc?id=1FZn-sOOgrMB_Oxion64eSGi7LAMo7h7u',
     nameItem: 'Cupcakes BlueBerry',
     description:
       'A massa do cupcake é feita com ingredientes tradicionais, como farinha de trigo, açúcar, manteiga e ovos, mas o destaque fica por conta das blueberries frescas ou congeladas, que são incorporadas à massa, adicionando um sabor único e pontinhos coloridos ao cupcake. Ao assar, o cupcake desenvolve uma textura leve e úmida, enquanto as blueberries liberam seus sucos, criando pequenas explosões de sabor em cada mordida. O contraste entre a maciez do cupcake e o toque frutado das blueberries torna esse cupcake uma opção deliciosa para os amantes de sobremesas. Para finalizar, pode-se adicionar uma cobertura de cream cheese ou buttercream com sabor de blueberry, realçando ainda mais o sabor da fruta e proporcionando uma experiência completa de sabores.',
@@ -56,10 +56,10 @@ export const data = [
   },
   {
     id: 5,
-    img: 'assets/img/strawberrie.png',
-    img2: 'assets/img/mg1.jpg',
-    img3: 'assets/img/mg2.jpg',
-    img4: 'assets/img/mg3.jpg',
+    img: 'https://drive.google.com/uc?id=1NIC-GvtYyA-XL7nzqC91Tupz61i5tx-7',
+    img2: 'https://drive.google.com/uc?id=1VwDt_bNCtfisG9nhZ5r4Yl4jM8ca1goo',
+    img3: 'https://drive.google.com/uc?id=10iE84H5GHT__1UCWIIln7-lfvWU4IO_e',
+    img4: 'https://drive.google.com/uc?id=1t79SGGhmAebkiXdzXjZSOb_TSc2wg2aw',
     nameItem: 'Cupcakes Morango',
     description:
       'O cupcake de morango é uma deliciosa sobremesa que combina a fofura e a umidade de um bolo com o sabor doce e refrescante dos morangos. É feito com uma massa macia e suave, geralmente aromatizada com baunilha, e recheado com um creme de morango cremoso. Na cobertura, é comum encontrar um delicioso buttercream de morango ou um ganache de chocolate branco com pedaços de morango. Decorado com um morango fresco ou polvilhado com migalhas de biscoito, o cupcake de morango é uma tentação irresistível para os amantes de doces, oferecendo uma explosão de sabores frutados e uma experiência indulgente para os paladares. É perfeito para comemorar ocasiões especiais, festas ou simplesmente para saborear um doce prazeroso a qualquer momento.',
@@ -69,10 +69,10 @@ export const data = [
   },
   {
     id: 6,
-    img: 'assets/img/banana.png',
-    img2: 'assets/img/bn1.jpg',
-    img3: 'assets/img/bn2.jpg',
-    img4: 'assets/img/bn3.jpg',
+    img: 'https://drive.google.com/uc?id=1Kd3ruRdSNpbcGxqD7h8GQ39k45FNLcij',
+    img2: 'https://drive.google.com/uc?id=1kEA4BAvJFv2fTv5DyeAI143DqP4TKoAr',
+    img3: 'https://drive.google.com/uc?id=1P9Qm6SziMUh4htndFcP8QBYOAgprBQZL',
+    img4: 'https://drive.google.com/uc?id=1yUPuG-EWGqNpFencfwhEJGTOsyvzTpmx',
     nameItem: 'Cupcakes Banana',
     description:
       'O cupcake de banana é uma delícia reconfortante que combina a suavidade e o sabor doce da banana madura com a textura macia e úmida de um bolo. Feito com uma massa aromática de banana, geralmente enriquecida com especiarias como canela e noz-moscada, o cupcake de banana é uma verdadeira explosão de sabores. Ele pode ser acompanhado por um delicioso recheio de banana cremoso e é frequentemente coberto com um irresistível cream cheese frosting ou uma ganache de chocolate. Com seu aroma delicioso e seu sabor reconfortante, o cupcake de banana é uma escolha perfeita para os amantes de sobremesas que desejam saborear algo aconchegante e cheio de sabor. É uma opção tentadora para desfrutar em lanches da tarde, festas ou como um mimo doce a qualquer momento.',
@@ -82,10 +82,10 @@ export const data = [
   },
   {
     id: 7,
-    img: 'assets/img/mango.png',
-    img2: 'assets/img/m1.jpg',
-    img3: 'assets/img/m2.png',
-    img4: 'assets/img/m3.png',
+    img: 'https://drive.google.com/uc?id=1FGcXr5jt5vks0JuZKZCr4hzHIzlmP5DI',
+    img2: 'https://drive.google.com/uc?id=1Jl4Uy_j-JStg1-CNzyqgOYjw49UhnZpK',
+    img3: 'https://drive.google.com/uc?id=1YQzN-Kz-z2Ku-zmZoGe70aQTuPT05N4j',
+    img4: 'https://drive.google.com/uc?id=12q99PrI09tF8dNRJDdBgqsVwTwqUGpcc',
     nameItem: 'Macarons de Manga',
     description:
       'O macaron de manga é uma pequena joia da confeitaria que combina a delicadeza de um biscoito crocante por fora com um recheio suave e cremoso de manga. Essa iguaria francesa encanta os amantes de doces com sua aparência colorida e elegante. O biscoito de amêndoa, leve e aerado, oferece uma textura única que contrasta perfeitamente com o creme de manga, que traz um sabor tropical e refrescante. Cada mordida é uma explosão de sabores tropicais, com notas doces e suculentas da manga combinadas com a sutileza das amêndoas. O macaron de manga é uma opção perfeita para quem busca uma sobremesa sofisticada e exótica, capaz de agradar tanto o paladar quanto os olhos. É uma verdadeira obra-prima da confeitaria que proporciona uma experiência irresistível e memorável para os amantes de doces.',
@@ -95,10 +95,10 @@ export const data = [
   },
   {
     id: 8,
-    img: 'assets/img/cerise.png',
-    img2: 'assets/img/cr1.jpg',
-    img3: 'assets/img/cr2.jpg',
-    img4: 'assets/img/cr3.jpg',
+    img: 'https://drive.google.com/uc?id=1XgwUaxd-9XUrec1RxvjyfzXY_bFzc6fg',
+    img2: 'https://drive.google.com/uc?id=1M5Fi4T71FxtrckJp7t-KmFIdVF8K1THs',
+    img3: 'https://drive.google.com/uc?id=1xGRYBorlN5Dk-rcO5JRycWUs66nA2zf2',
+    img4: 'https://drive.google.com/uc?id=1NgaoP7NtV1yWobrer_e7_8rtvwlZE8I9',
     nameItem: 'Macarons de Cereja',
     description:
       'O macaron de cereja é uma deliciosa iguaria francesa que combina a leveza e crocância do biscoito de amêndoa com um recheio suave e aromático de cereja. Com sua aparência encantadora e cores vibrantes, o macaron de cereja atrai os amantes de doces com sua combinação única de sabores. O biscoito de amêndoa cria uma base perfeita para ressaltar o sabor do recheio de cereja, que é doce, frutado e levemente ácido. Cada mordida é uma experiência gustativa equilibrada, com a textura delicada do biscoito contrastando com a explosão de sabor da cereja. O macaron de cereja é uma opção irresistível para aqueles que desejam apreciar uma sobremesa refinada e cheia de sabores intensos. É uma verdadeira tentação para os amantes de doces que buscam uma combinação elegante e prazerosa de sabores.',
@@ -108,10 +108,10 @@ export const data = [
   },
   {
     id: 9,
-    img: 'assets/img/coffee.png',
-    img2: 'assets/img/cf1.jpg',
-    img3: 'assets/img/cf2.jpg',
-    img4: 'assets/img/cf3.jpg',
+    img: 'https://drive.google.com/uc?id=1Y0tuAGcjHldq4-oD0qey6jP1sHYCr_SK',
+    img2: 'https://drive.google.com/uc?id=1GrolLwgf6TZDEmM5Qce96iIYpS6vLjFP',
+    img3: 'https://drive.google.com/uc?id=1he6P-WafZDzeVpq7Bfvr3meONiuBEJ-x',
+    img4: 'https://drive.google.com/uc?id=1uDdzbu8DXW21kRJjvvU6Y2uTRX3gy8Zw',
     nameItem: 'Macarons de Café',
     description:
       'O macaron de café é uma delícia para os amantes da bebida aromática e intensa. Com uma combinação perfeita entre a crocância do biscoito de amêndoa e o sabor marcante do café, esse pequeno doce encanta os paladares mais exigentes. Cada mordida revela uma textura delicada e um equilíbrio perfeito entre o sabor do café e a suavidade do recheio. O aroma envolvente do café permeia todo o macaron, despertando os sentidos e proporcionando uma experiência única. Seja como acompanhamento para uma xícara de café ou como um deleite independente, o macaron de café é uma verdadeira indulgência para os amantes dessa bebida tão amada. Sua sofisticação e sabor inconfundível fazem dele uma opção irresistível para os apreciadores de doces e café.',
